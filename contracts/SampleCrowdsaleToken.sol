5d2bdc02bfcd4e8c187b46d648569791bac74a66
unknown <peturko@gmail.com>
2018-04-02 13:46:24 +0000

    Adjusting contracts for windows

--- contracts/SampleCrowdsaleToken.sol
+++ contracts/SampleCrowdsaleToken.sol
@@ -1,9 +1,9 @@
-pragma solidity 0.4.19;
+pragma solidity ^0.4.21;
 
-import '../zeppelin-solidity/contracts/token/ERC20/MintableToken.sol';
+import "../node_modules/zeppelin-solidity/contracts/token/ERC20/MintableToken.sol";
 
 contract SampleCrowdsaleToken is MintableToken {
-  string public name = "AID COIN";
-  string public symbol = "AID";
-  uint256 public decimals = 18;
+    string public name = "AID COIN";
+    string public symbol = "AID";
+    uint256 public decimals = 18;
 }
