745188040c4447243dab626a7d32bce8245d939c
eliasingi12 <elias12@ru.is>
2018-04-26 17:37:30 +0000

    add

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -4,7 +4,7 @@ const DisasterRelief = artifacts.require('./DisasterRelief.sol');
 module.exports = function(deployer, network, accounts) {
     const openingTime = web3.eth.getBlock('latest').timestamp + 20; // twenty secs in the future
     const closingTime = openingTime + 1800; // 30 min
-    const rate = new web3.BigNumber; // the rate is 1 token per 1 Eth
+    const rate = new web3.BigNumber(1); // the rate is 1 token per 1 Eth
 
     return deployer
         .then(() => {
