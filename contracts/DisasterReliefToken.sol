ac3eaedff188ece81b1b8eda2fccca1777c69f88
brynjars88 <brynjar.sig88@gmail.com>
2018-04-27 10:38:37 +0000

    Added MintableToken and changed one import

--- contracts/DisasterReliefToken.sol
+++ contracts/DisasterReliefToken.sol
@@ -1,6 +1,6 @@
 pragma solidity ^0.4.21;
 
-import "../node_modules/zeppelin-solidity/contracts/token/ERC20/MintableToken.sol";
+import "./Resources/token/MintableToken.sol";
 
 contract DisasterReliefToken is MintableToken {
     string public name = "Takk COIN";
