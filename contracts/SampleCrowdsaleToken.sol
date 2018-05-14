1f093351733b08538cd65e4245db21ef7e84f79c
annavigdis <annaviru@gmail.com>
2018-05-14 19:08:29 +0000

    remove samplecrowdsaletoken contract and moved it into the samplecrowdsale contract:

--- contracts/SampleCrowdsaleToken.sol
+++
@@ -1,9 +0,0 @@
-pragma solidity ^0.4.18;
-
-import "./MintableToken.sol";
-
-contract SampleCrowdsaleToken is MintableToken {
-    string public name = "Takk COIN";
-    string public symbol = "Takk";
-    uint256 public decimals = 18;
-}
